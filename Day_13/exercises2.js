e829cbfc249e85ee24da914fd6570a9e41db47dc
Arden Silva <arsilva@utexas.edu>
2023-12-26 16:40:21 -0600

    day 13 - exercises 2

---
+++ Day_13/exercises2.js
@@ -0,0 +1,9 @@
+console.log('Day 13 - Exercises 2')
+//10 > 2 * 10 use console.assert()
+console.assert(10 > 2 * 10,'2*10 is not bigger than 10!')
+
+//Write a warning message using console.warn()
+console.warn('Console Warning')
+
+//Write an error message using console.error()
+console.error('Console Error')
