26bf0f745b438cd28d714086fd7674bf988f957d
Arden Silva <arsilva@utexas.edu>
2023-12-26 16:03:41 -0600

    day 12 - exercises 2

--- Day_12/exercises1.js
+++ Day_12/exercises1.js
@@ -1,4 +1,4 @@
-console.log('Day 12 - Exercises1')
+console.log('Day 12 - Exercises 1')
 
 //Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
 const incomeTxt =  'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
---
+++ Day_12/exercises2.js
@@ -0,0 +1,35 @@
+console.log('Day 12 - Exercises 2')
+//Write a function called tenMostFrequentWords which get the ten most frequent word from a string?
+
+const paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
+function tenMostFrequentWords(paragraph, length = paragraph.length){
+    const frq = []
+    const spacePattern = /[^ .]+/g
+    const match = paragraph.match(spacePattern)
+    
+    for (word of match){
+        const wordsFound = []
+        for (item of frq){
+            wordsFound.push(item.word)
+        }
+        if(!wordsFound.includes(word)){
+            frq.push({word:word, count:1})
+        } else{
+            for (item in frq){
+                if(frq[item].word === word){
+                    frq[item].count = 1 + frq[item].count
+                }
+            }
+        }
+    }
+    frq.sort()
+    frq.sort((a,b)=>{
+        if(a.count > b.count) return -1
+        if(a.count < b.count) return 1
+        return 0
+    })
+    return(frq.slice(0,length))
+}
+
+console.log(tenMostFrequentWords(paragraph))
+console.log(tenMostFrequentWords(paragraph,10))
