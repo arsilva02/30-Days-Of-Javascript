4347a3e100abccc551c2692d66616a2f17e56c80
Arden Silva <arsilva@utexas.edu>
2023-12-27 17:14:39 -0600

    day 16 - notes

---
+++ Day_16/notes.js
@@ -0,0 +1,131 @@
+console.log('Day 16 - Notes')
+//json is javascript object notation
+//string/test only, light weight data format for storing and transporting
+//mostly used when data is sent from a server to a client
+
+
+//keys need to have double quotes
+//lets use json parse
+const usersTextJSON = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`
+const usersTest = JSON.parse(usersTextJSON,undefined,4)
+console.log(usersTest)
+
+//using reviver
+const usersReviver = JSON.parse(usersTextJSON, (key, value) => {
+    let newValue =
+      typeof value == 'string' && key != 'email' ? value.toUpperCase() : value //caps all non email strings
+    return newValue
+  })
+console.log(usersReviver)
+
+
+//converting object to json
+const usersObject = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'Redux',
+        'MongoDB',
+        'Express',
+        'React',
+        'Node'
+      ],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'MongoDB',
+        'Express',
+        'React',
+        'Node'
+      ],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+  }
+  
+const userstxt = JSON.stringify(usersObject, undefined, 4) //we can just use undefined since we are not filtering out
+console.log(userstxt) // text means JSON- because json is a string form of an object.
+
+//filter array
+const singleUser = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    country: 'Finland',
+    city: 'Helsinki',
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Python'],
+    age: 250,
+    isLoggedIn: false,
+    points: 30
+  }
+  
+
+const filteredUserstxt = JSON.stringify(singleUser,['firstName', 'lastName', 'country', 'city', 'age'],4)
+console.log(filteredUserstxt)
