0f124a90e90713ef6c0258defdda8effc081a0a1
Arden Silva <arsilva@utexas.edu>
2023-12-29 15:43:29 -0600

    day 17 - ex 1

---
+++ Day_17/exercises1.js
@@ -0,0 +1,9 @@
+console.log('Day 17 - Exercises 1')
+localStorage.clear()
+//Store you first name, last name, age, country, city in your browser localStorage.
+localStorage.setItem('firstName', 'Arden')
+localStorage.setItem('lastName', 'Silva')
+localStorage.setItem('age', 21)
+localStorage.setItem('country', 'USA')
+localStorage.setItem('city', 'Austin')
+console.log(localStorage)
