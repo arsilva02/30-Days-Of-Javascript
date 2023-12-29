b6f2e27276ce358018d1b923f1e6e188a606a975
Arden Silva <arsilva@utexas.edu>
2023-12-29 16:36:42 -0600

    Update exercises1.js

--- Day_18/exercises1.js
+++ Day_18/exercises1.js
@@ -1,1 +1,16 @@
 console.log('Day 18 - Exercises 1')
+//Read the countries API using fetch and print the name of country, capital, languages, population and area.
+const countriesAPI = 'https://restcountries.com/v2/all'
+const countryFetch = async() =>{
+    try{
+        const response = await fetch (countriesAPI)
+        const countries = await response.json()
+        countries.forEach(c => {
+            console.log(c.name, c.capital, c.languages,c.population,c.area)
+        });
+    } catch(e){
+        console.error(e)
+    }
+}
+console.log('===== async and await')
+countryFetch()
